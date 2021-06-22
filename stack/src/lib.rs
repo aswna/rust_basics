b84565308ce69ba3654b60ed3f8a6b4b1a9e7d65
aswna <aswna@server.fake>
2021-06-22 09:09:55 +0200

    Small steps in unit testing Stack

--- stack/src/lib.rs
+++ stack/src/lib.rs
@@ -1,24 +1,37 @@
-struct Stack {
-    pub max_size : usize,
+struct Stack<T> {
+    pub max_size: usize,
+    pub elements: Vec<T>,
 }
 
-// impl Stack {
-//     pub fn with_max_size(max_size: usize) -> Self {
-//         Self {
-//             max_size,
-//         }
-//     }
-// }
+impl<T> Stack<T> {
+    pub fn new(size: usize) -> Self {
+        Self {
+            max_size: size,
+            elements: Vec::new(),
+
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_stack_struct_can_be_instantiated() {
-        let a_stack = Stack{max_size: 7};
-        // let a_stack = Stack::with_max_size(7);
-        let size = a_stack.max_size;
-        println!("max size is {}", size)
+    fn test_stack_can_be_instantiated_with_string_type() {
+        let a_stack = Stack::<String>::new(7);
+        assert_eq!(7, a_stack.max_size);
     }
+
+    #[test]
+    fn test_stack_can_be_instantiated_with_i32_type() {
+        let a_stack = Stack::<i32>::new(13);
+        assert_eq!(13, a_stack.max_size);
+    }
+
+    // TODO: check elements type correctness
+
+    // #[test]
+    // fn test_top_on_empty_stack_returns_null() {
+    // }
 }
